11fdfc3a26be8362a3c87f1d2410bc5231a756dc
Mehmetcan Budak <mehmetcanbudak@gmail.com>
2021-04-22 21:52:30 +0300

    Added Blog page and styled the homepage.

---
+++ pages/blog.js
@@ -0,0 +1,4 @@
+function BlogPage() {
+  return <div className="site-container">hello</div>
+}
+export default BlogPage
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
 function HomePage() {
-  return <div className="text-purple-500 bg-blue-100">Welcome to Next.js!</div>
+  return (
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="text-2xl font-bold">
+            Hi! I'm Mehmetcan Budak. Living in Istanbul.
+          </h1>
+
+          <p>
+            Cryptocurrencies, statistics, data science and blockchain
+            technologies. Electronic music, modular synthesis and creative
+            design.
+          </p>
+
+          <p>Feel free to check out my blog for more info.</p>
+        </div>
+      </div>
+
+      <div className="max-w-4xl mx-auto mt-20">
+        <img src="/1.jpg" alt="" />
+      </div>
+    </>
+  )
 }
 
 export default HomePage
